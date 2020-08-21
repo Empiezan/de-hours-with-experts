fae7c8e484b2ae8d9b7b31e3e0f0751ea065dce5
Michael Chang <michaelchang90@gmail.com>
2020-08-20 23:17:57 -0500

    Implemented nextBiggestNumber method
    Passing all 150 tests in TestNextBiggestNumberTest

--- java/src/main/java/com/labs1904/NextBiggestNumber.java
+++ java/src/main/java/com/labs1904/NextBiggestNumber.java
@@ -1,5 +1,7 @@
 package com.labs1904;
 
+import java.util.PriorityQueue;
+
 public class NextBiggestNumber {
 
     public static void main(String[] args) {
@@ -10,7 +12,53 @@ public class NextBiggestNumber {
     }
 
     public static int getNextBiggestNumber(Integer i) {
-        //TODO: IMPLEMENT ME!
-        return 0;
+        // Store the original value to compare with next biggest #
+        int originalNum = i;
+        int nextBiggestNum = originalNum;
+        // The digit that will replace a more significant digit `digitToDemote`, if a larger # exists
+        int digitToSwap = -1;
+        // The digit that will be replaced by `digitToSwap`
+        int digitToDemote = originalNum % 10;
+        // Min heap to sort the digits less significant than `digitToDemote`
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+
+        // Find digitToDemote
+        int numberOfDigits = (int) Math.log10(originalNum) + 1;
+        // The digits that are more significant than digitToDemote
+        int prefix = originalNum;
+        do {
+            int currentDigit = prefix % 10;
+            minHeap.add(currentDigit);
+            prefix /= 10;
+
+            boolean foundDigitToDemote = currentDigit < digitToDemote;
+            digitToDemote = currentDigit;
+            if (foundDigitToDemote) break;
+        }
+        while (prefix != 0);
+
+        // Find digitToSwap
+        for (int index = 0; index < numberOfDigits; index++) {
+            int currentDigit = nextBiggestNum % 10;
+            boolean foundDigitToSwap = currentDigit > digitToDemote && currentDigit > digitToSwap;
+            digitToSwap = currentDigit;
+            if (foundDigitToSwap) break;
+            nextBiggestNum /= 10;
+        }
+
+        // Build next largest number
+        nextBiggestNum = prefix * 10 + digitToSwap;
+        while (!minHeap.isEmpty()) {
+            int nextDigit = minHeap.remove();
+            if (nextDigit == digitToSwap) {
+                digitToSwap = -1;
+                continue;
+            }
+            nextBiggestNum = nextBiggestNum * 10 + nextDigit;
+        }
+
+        return nextBiggestNum <= originalNum ? -1 : nextBiggestNum;
     }
+
+
 }
